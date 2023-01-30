c7d76505c5db0d60b6b34f4dd70e06e6173d6c0d
zhi <zhi1107outlook.com>
2023-01-30 19:43:12 +0800

    master commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello master!");
     }
 }
